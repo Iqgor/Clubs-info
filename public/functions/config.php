f28684ece8c02eade89606dd697f653d7ee76fb3
Iqgor <herrebrughigor@gmail.com>
2023-10-17 10:59:01 +0200

    eerste commit

---
+++ public/functions/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+$config = [
+    'server' => 'mariadb',
+    'database' => 'newsletters',
+    'username' => 'root',
+    'password' => '4dy5qwtrsag#!sad',
+    'port' =>  3306 
+
+]; 
+
+
+return $config;
+?>
